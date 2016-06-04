baa3d97cac71d52b49515899519b0d993b0f0347
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-06-04 17:09:47 +0530

    Starter code courtesy of Udacity. Update dependencies, package names, and organization.

--- app/src/androidTest/java/com/sudhirkhanger/app/stockhawk/ApplicationTest.java
+++ app/src/androidTest/java/com/sudhirkhanger/app/stockhawk/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.sam_chordas.android.stockhawk;
+package com.sudhirkhanger.app.stockhawk;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/com/sudhirkhanger/app/stockhawk/model/QuoteDatabase.java
+++ app/src/main/java/com/sudhirkhanger/app/stockhawk/model/QuoteDatabase.java
@@ -1,4 +1,4 @@
-package com.sam_chordas.android.stockhawk.data;
+package com.sudhirkhanger.app.stockhawk.model;
 
 import net.simonvt.schematic.annotation.Database;
 import net.simonvt.schematic.annotation.Table;
@@ -8,9 +8,11 @@ import net.simonvt.schematic.annotation.Table;
  */
 @Database(version = QuoteDatabase.VERSION)
 public class QuoteDatabase {
-  private QuoteDatabase(){}
+    private QuoteDatabase() {
+    }
 
-  public static final int VERSION = 7;
+    public static final int VERSION = 7;
 
-  @Table(QuoteColumns.class) public static final String QUOTES = "quotes";
+    @Table(QuoteColumns.class)
+    public static final String QUOTES = "quotes";
 }
