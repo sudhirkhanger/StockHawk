edc59f83dfa18e198f87ade3e3a2f2b52b493c30
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-06-11 08:19:57 +0530

    hack retrofit historical data

--- app/src/main/java/com/sudhirkhanger/app/stockhawk/ui/MyStockGraphActivity.java
+++ app/src/main/java/com/sudhirkhanger/app/stockhawk/ui/MyStockGraphActivity.java
@@ -16,17 +16,33 @@
 
 package com.sudhirkhanger.app.stockhawk.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.sudhirkhanger.app.stockhawk.R;
+import com.sudhirkhanger.app.stockhawk.model.Stock;
+import com.sudhirkhanger.app.stockhawk.rest.StockService;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyStockGraphActivity extends Activity {
+
+    private static final String BASE_URL = "https://query.yahooapis.com/";
+    private Call<Stock> mStockServiceCall;
+    private Stock mStock;
+    private List<Stock.StockItem> mStockItem;
 
     @BindView(R.id.testTextView)
     TextView mTextView;
@@ -41,9 +57,48 @@ public class MyStockGraphActivity extends Activity {
 
         Intent intent = getIntent();
 
-        String str = intent.getStringExtra("stock_symbol");
+        String symbol = intent.getStringExtra("stock_symbol");
 
-        mTextView.setText(str);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        StockService.StockYql stockYql = retrofit.create(StockService.StockYql.class);
+
+        String startDate = "2015-06-10";
+        String endDate = "2016-06-10";
+
+        String query = "select * from yahoo.finance.historicaldata where symbol = \'" + symbol + "\' and startDate = \'" + startDate + "\' and endDate = \'" + endDate + "\'";
+
+        mStockServiceCall = stockYql.getHistoricalData(query);
+        mStockServiceCall.enqueue(new Callback<Stock>() {
+            @Override
+            public void onResponse(Call<Stock> call, Response<Stock> response) {
+
+                try {
+                    mStock = response.body();
+                    Log.d("MyStockGraphActivity", response.toString());
+                } catch (NullPointerException e) {
+                    Toast toast = null;
+                    if (response.code() == 401) {
+                        toast = Toast.makeText(MyStockGraphActivity.this, "Unauthenticated", Toast.LENGTH_SHORT);
+                    } else if (response.code() >= 400) {
+                        toast = Toast.makeText(MyStockGraphActivity.this, "Client Error " + response.code()
+                                + " " + response.message(), Toast.LENGTH_SHORT);
+                    }
+                    toast.show();
+                }
+//                mStockItem = mStock.getStockItems();
+//                mTextView.setText(mStockItem.get(0).getClose());
+            }
+
+            @Override
+            public void onFailure(Call<Stock> call, Throwable t) {
+                Log.d("MyStockGraphActivity", t.getMessage());
+            }
+        });
+
     }
 
 }
