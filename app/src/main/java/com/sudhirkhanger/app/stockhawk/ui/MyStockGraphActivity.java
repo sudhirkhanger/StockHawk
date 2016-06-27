4eae4959b42b1552dea2e35fb032e91173e536f3
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-06-27 18:36:23 +0530

    fetches data and shows graph

--- app/src/main/java/com/sudhirkhanger/app/stockhawk/ui/MyStockGraphActivity.java
+++ app/src/main/java/com/sudhirkhanger/app/stockhawk/ui/MyStockGraphActivity.java
@@ -16,12 +16,16 @@
 
 package com.sudhirkhanger.app.stockhawk.ui;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sudhirkhanger.app.stockhawk.R;
@@ -29,9 +33,9 @@ import com.sudhirkhanger.app.stockhawk.model.Stock;
 import com.sudhirkhanger.app.stockhawk.rest.QuoteDeserializer;
 import com.sudhirkhanger.app.stockhawk.rest.StockService;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -39,27 +43,30 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MyStockGraphActivity extends Activity {
+public class MyStockGraphActivity extends AppCompatActivity {
 
     private static final String BASE_URL = "https://query.yahooapis.com/";
     private Call<Stock> mStockServiceCall;
     private Stock mStock;
-    private List<Stock.StockItem> mStockItem;
-
-    @BindView(R.id.testTextView)
-    TextView mTextView;
+    private List<Stock.StockItem> mStockItemList;
+    private int dataSetSize;
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_my_stock_graph);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
+        lineChart = (LineChart) findViewById(R.id.linechart);
 
+        Intent intent = getIntent();
         String symbol = intent.getStringExtra("stock_symbol");
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(symbol);
+        }
 
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(Stock.class, new QuoteDeserializer())
@@ -84,20 +91,18 @@ public class MyStockGraphActivity extends Activity {
 
                 try {
                     mStock = response.body();
-                    mStockItem = mStock.getStockItems();
-
-                    Log.d("MyStockGraphActivity", "Size " + mStockItem.size());
+                    mStockItemList = mStock.getStockItems();
+                    dataSetSize = mStockItemList.size();
+                    realChart();
                 } catch (NullPointerException e) {
-//                    Toast toast = null;
+                    Toast toast = null;
                     if (response.code() == 401) {
-                        Log.d("MyStockGraphActivity", " 401 Unauthenticated");
-//                        toast = Toast.makeText(MyStockGraphActivity.this, "Unauthenticated", Toast.LENGTH_SHORT);
+                        toast = Toast.makeText(MyStockGraphActivity.this, "Unauthenticated", Toast.LENGTH_SHORT);
                     } else if (response.code() >= 400) {
-                        Log.d("MyStockGraphActivity", "400 Client Error " + response.code() + " " + response.message());
-//                        toast = Toast.makeText(MyStockGraphActivity.this, "Client Error " + response.code()
-//                                + " " + response.message(), Toast.LENGTH_SHORT);
+                        toast = Toast.makeText(MyStockGraphActivity.this, "Client Error " + response.code()
+                                + " " + response.message(), Toast.LENGTH_SHORT);
                     }
-//                    toast.show();
+                    toast.show();
                 }
             }
 
@@ -107,5 +112,23 @@ public class MyStockGraphActivity extends Activity {
             }
         });
 
+    }
+
+    private void realChart() {
+        ArrayList<String> xVals = new ArrayList<>();
+        ArrayList<Entry> yVals = new ArrayList<>();
+
+        for (int i = 0; i < dataSetSize; i++) {
+            xVals.add(i, mStockItemList.get(i).getDate());
+            yVals.add(new Entry(Float.valueOf(mStockItemList.get(i).getClose()), i));
+        }
+
+        LineDataSet dataSet = new LineDataSet(yVals, "Close");
+
+        LineData lineData = new LineData(xVals, dataSet);
+        lineChart.setDescription("Closing values over time");
+        dataSet.setDrawFilled(true);
+        lineChart.setData(lineData);
+        lineChart.animateY(5000);
     }
 }
