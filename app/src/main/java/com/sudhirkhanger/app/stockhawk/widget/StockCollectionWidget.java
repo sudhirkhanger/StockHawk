9360a741868ea99068e79178a401367d08a0809a
Sudhir Khanger <sudhir@sudhirkhanger.com>
2016-06-29 10:50:49 +0530

    add widget code from udacity widget webcast

---
+++ app/src/main/java/com/sudhirkhanger/app/stockhawk/widget/StockCollectionWidget.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2016 Sudhir Khanger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sudhirkhanger.app.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+import com.sudhirkhanger.app.stockhawk.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class StockCollectionWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.stock_collection_widget);
+
+        // Set up the collection
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            setRemoteAdapter(context, views);
+        } else {
+            setRemoteAdapterV11(context, views);
+        }
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, WidgetService.class));
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    @SuppressWarnings("deprecation")
+    private static void setRemoteAdapterV11(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(0, R.id.widget_list,
+                new Intent(context, WidgetService.class));
+    }
+}
+
---
+++ app/src/main/java/com/sudhirkhanger/app/stockhawk/widget/WidgetDataProvider.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2016 Sudhir Khanger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sudhirkhanger.app.stockhawk.widget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
+
+    List<String> collection = new ArrayList<>();
+    Context mContext;
+    Intent mIntent;
+
+    public WidgetDataProvider(Context context, Intent intent) {
+        mContext = context;
+        mIntent = intent;
+    }
+
+    private void intiData() {
+        collection.clear();
+        for (int i = 1; i <= 10; i++) {
+            collection.add("ListView Item " + i);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        intiData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        intiData();
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return collection.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(),
+                android.R.layout.simple_list_item_1);
+        remoteViews.setTextViewText(android.R.id.text1, collection.get(i));
+        remoteViews.setTextColor(android.R.id.text1, Color.BLACK);
+        return remoteViews;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+}
